aeb062b8f5f3e6c0030662b54e4d65864a3a603a
Joan Andrés <jhoan.buritica00@usc.edu.co>
2021-04-07 20:50:57 -0500

    Update SQL Script for Car

--- Docs/Car.sql
+++ Docs/Car.sql
@@ -1,11 +1,16 @@
 create table Car
 (
-    CAR_LICENSE       varchar(6)   not null,
-    CAR_IMAGE         varchar(250) not null,
-    CAR_COPYRIGHT     varchar(80)  null,
-    CAR_OWN           int          not null,
-    CAR_NAME          varchar(80)  not null,
-    CAR_PRICE_PER_DAY int          not null,
+    CAR_LICENSE       varchar(6)                                            not null,
+    CAR_IMAGE         varchar(250)                                          not null,
+    CAR_COPYRIGHT     varchar(80)                                           null,
+    CAR_OWN           int                                                   not null,
+    CAR_NAME          varchar(80)                                           not null,
+    CAR_PRICE_PER_DAY int                                                   not null,
+    CAR_BAGS          smallint                                              null,
+    CAR_DOORS         smallint                                              null,
+    CAR_PEOPLE        smallint                                              null,
+    CAR_TRANSMISSION  enum ('Manual', 'Automatic')                          null,
+    CAR_AIR           enum ('Air-conditioning', 'Without Air-conditioning') null,
     constraint Car_CAR_PLACA_uindex
         unique (CAR_LICENSE),
     constraint CAR_OWN_SERIAL
