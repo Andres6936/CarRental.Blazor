36a962f1ddb96926692368cbd8b82727185eb94f
Joan Andrés <jhoan.buritica00@usc.edu.co>
2021-04-20 22:46:12 -0500

    Added news tables

---
+++ Docs/Cliente.sql
@@ -0,0 +1,18 @@
+create table Cliente
+(
+    CLI_USER               varchar(15) not null,
+    CLI_PASSWORD           varchar(80) not null,
+    CLI_EMAIL              varchar(80) not null,
+    CLI_FIRST_NAME         varchar(80) not null,
+    CLI_LAST_NAME          varchar(80) null,
+    CLI_CREDIT_CARD_NUMBER varchar(22) null,
+    CLI_CREDIT_CARD_CVV    int         null,
+    CLI_COUNTRY            varchar(80) null,
+    CLI_PHONE              varchar(12) null,
+    constraint Cliente_CLI_USER_uindex
+        unique (CLI_USER)
+);
+
+alter table Cliente
+    add primary key (CLI_USER);
+
---
+++ Docs/Loan.sql
@@ -0,0 +1,18 @@
+create table Loan
+(
+    LOAN_SERIAL      int auto_increment,
+    LOAN_USER        varchar(15) not null,
+    LOAN_DATE_START  date        not null,
+    LOAN_DATE_END    date        not null,
+    LOAN_CAR_LICENSE varchar(6)  not null,
+    constraint Loan_LOAN_SERIAL_uindex
+        unique (LOAN_SERIAL),
+    constraint Loan_CAR_LICENSE
+        foreign key (LOAN_CAR_LICENSE) references Car (CAR_LICENSE),
+    constraint Loan_CLI_USER
+        foreign key (LOAN_USER) references Cliente (CLI_USER)
+);
+
+alter table Loan
+    add primary key (LOAN_SERIAL);
+
