6c47f326d2cffe80067714b53201cf1686ffc295
Joan Andrés <jhoan.buritica00@usc.edu.co>
2021-04-21 21:06:39 -0500

    Added Client Model

---
+++ Data/Client.cs
@@ -0,0 +1,23 @@
+namespace Rental.Data
+{
+    public class Client
+    {
+        public string User { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string CreditCardNumber { get; set; }
+
+        public string CreditCardCvv { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
