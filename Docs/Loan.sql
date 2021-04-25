d85ddb3341249251289982f78cca1b07ac64ba00
Joan Andrés <jhoan.buritica00@usc.edu.co>
2021-04-25 17:25:35 -0500

    Updated Database

--- Docs/Loan.sql
+++ Docs/Loan.sql
@@ -1,11 +1,12 @@
 create table Loan
 (
-    LOAN_SERIAL      int auto_increment,
-    LOAN_USER        varchar(15)            not null,
-    LOAN_DATE_START  date                   not null,
-    LOAN_DATE_END    date                   not null,
-    LOAN_CAR_LICENSE varchar(6)             not null,
-    LOAN_HB_CANCELED enum ('TRUE', 'FALSE') not null,
+    LOAN_SERIAL          int auto_increment,
+    LOAN_USER            varchar(15)            not null,
+    LOAN_DATE_START      date                   not null,
+    LOAN_DATE_END        date                   not null,
+    LOAN_CAR_LICENSE     varchar(6)             not null,
+    LOAN_HB_CANCELED     enum ('TRUE', 'FALSE') not null,
+    LOAN_TOTAL_DAYS_USED int                    not null,
     constraint Loan_LOAN_SERIAL_uindex
         unique (LOAN_SERIAL),
     constraint Loan_CAR_LICENSE
