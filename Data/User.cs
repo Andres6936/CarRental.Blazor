e7261719305d80f6c9c255791496eeef84927e1f
Joan Andrés <jhoan.buritica00@usc.edu.co>
2021-04-30 21:04:24 -0500

    Removed unused attribute

--- Data/User.cs
+++ Data/User.cs
@@ -4,8 +4,6 @@ namespace Rental.Data
     {
         public string Username { get; set; }
 
-        public string Password { get; set; }
-
         public string Role { get; set; }
         
         public string Email { get; set; }
